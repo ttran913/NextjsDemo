5b03bf21ba48d9c31c4a42a60e75492729f2dfc5
ttran913 <122418320+ttran913@users.noreply.github.com>
2024-07-27 19:32:40 +0000

    Removed 'use client' fromsidenav.tsx

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -28,7 +28,7 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <a
+          <Link
             key={link.name}
             href={link.href}
             className={clsx(
@@ -40,7 +40,7 @@ export default function NavLinks() {
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
-          </a>
+          </Link>
         );
       })}
     </>
